32c065b9fd929056e760d6365f46f385ecf17eab
yeonyaily <yeonye330@gmail.com>
2024-01-25 23:44:31 +0900

    First Commit

---
+++ lib/common/layout/default_layout.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DefaultLayout extends StatelessWidget {
+  const DefaultLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
