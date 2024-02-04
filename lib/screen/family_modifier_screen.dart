455b74074676186acf4d332a85a735a8c273479b
yeonyaily <yeonye330@gmail.com>
2024-02-04 18:16:44 +0900

    [UPDATE] family modifier provider

--- lib/screen/family_modifier_screen.dart
+++ lib/screen/family_modifier_screen.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:test_riverpod/common/layout/default_layout.dart';
+import 'package:test_riverpod/riverpod/family_modifier_provider.dart';
 
-class FamilyModifierScreen extends StatelessWidget {
+class FamilyModifierScreen extends ConsumerWidget {
   const FamilyModifierScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final state = ref.watch(familyModifierProvider(3));
     return DefaultLayout(
-        title: 'FamilyModifierScreen',
-        body: Column(
-          children: [],
-        ));
+      title: 'FamilyModifierScreen',
+      body: Center(
+        child: state.when(
+          data: (data) => Text(data.toString()),
+          error: (err, stack) => Text(
+            err.toString(),
+          ),
+          loading: () => CircularProgressIndicator(),
+        ),
+      ),
+    );
   }
 }
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:test_riverpod/common/layout/default_layout.dart';
+import 'package:test_riverpod/screen/family_modifier_screen.dart';
 import 'package:test_riverpod/screen/future_provider_screen.dart';
 import 'package:test_riverpod/screen/state_notifier_provider_screen.dart';
 import 'package:test_riverpod/screen/state_provider_screen.dart';
@@ -53,6 +54,16 @@ class HomeScreen extends StatelessWidget {
               );
             },
             child: const Text('StreamProviderScreen'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => const FamilyModifierScreen(),
+                ),
+              );
+            },
+            child: const Text('FamilyModifierScreen'),
           ),
         ],
       ),
